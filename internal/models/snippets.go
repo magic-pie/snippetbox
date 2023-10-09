05699abe9b7b0a15dd5439222361148ec58cb6a8
magic-pie <36836440+magic-pie@users.noreply.github.com>
2023-10-09 22:36:23 +0700

    4.8. Multiple-record SQL queries

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -52,5 +52,32 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	stmt := `select id, title, content, created, expires from snippets where expires > utc_timestamp() order by id desc limit 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+
+		if err = rows.Err(); err != nil {
+			return nil, err
+		}
+
+		return snippets, nil
+	}
+
 	return nil, nil
 }
