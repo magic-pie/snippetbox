fbc94be19cc9a644815197b069955ae2c4576645
magic-pie <36836440+magic-pie@users.noreply.github.com>
2023-10-21 15:02:15 +0700

    17.1. Add an about page to the application

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -192,3 +192,15 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestAboutPageView(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/about")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.StringContains(t, body, "About page")
+}
