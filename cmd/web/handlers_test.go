96798b53bc36d052b056c833a86ba949fc10b425
magic-pie <36836440+magic-pie@users.noreply.github.com>
2023-10-21 15:43:12 +0700

    17.3. Test the snippetCreate handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -204,3 +204,32 @@ func TestAboutPageView(t *testing.T) {
 	assert.Equal(t, code, http.StatusOK)
 	assert.StringContains(t, body, "About page")
 }
+
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		code, _, body := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, `<form action="/snippet/create" method="POST">`)
+	})
+}
